135b95fc9310ba4a186c70cf67c0fb4f32156fe7
kMac <kmac@kMacs-MacBook-Air.local>
2026-09-19 18:51:43 +0700

    add homepage

--- app/urls.py
+++ app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from homepage.views import HomepageView
 
 urlpatterns = [
+    path('', HomepageView.as_view(), name='homepage'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
